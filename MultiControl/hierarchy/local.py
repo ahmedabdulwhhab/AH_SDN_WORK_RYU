370a0d3969c8f51fd58ad0e73daafb83b19c9d14
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-01-09 11:26:52 +0200

    Add files via upload

---
+++ MultiControl/hierarchy/local.py
@@ -0,0 +1,266 @@
+# -*- coding: utf-8 -*-
+import logging
+import networkx as nx
+from ryu.base import app_manager
+from ryu.ofproto import ofproto_v1_3
+from ryu.topology import api
+from ryu.topology.switches import LLDPPacket
+from ryu.lib.packet import packet, ethernet
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER
+
+import local_lib
+
+LOG = logging.getLogger(__name__)
+OFPPC_NO_FLOOD = 1 << 4
+mDNS = ['33:33:00:00:00:fb', '01:00:5E:00:00:FB']
+multicast_list = ["33:33:00:00:00:%02X" % x for x in range(0, 256)]
+
+
+class LocalControllerApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {
+        'local_lib': local_lib.LocalControllerLib
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(LocalControllerApp, self).__init__(*args, **kwargs)
+        self.local_lib = kwargs['local_lib']
+        # self.local_lib = local_lib.LocalControllerLib('127.0.0.1', 10807)
+        self.local_lib.start_serve('127.0.0.1', 10807)
+        self.global_port = {}
+        self.route_list = []
+        self.graph = None
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+
+        try:
+            # src: from other switch
+            src_dpid, src_port_no = LLDPPacket.lldp_parse(msg.data)
+            # dst: this switch
+            dst_dpid, dst_port_no = datapath.id, msg.match['in_port']
+
+            if src_dpid > 1024 or dst_dpid > 1024:
+                # hack: ignote illegal switch
+                return
+
+            switch = api.get_switch(self, src_dpid)
+
+            # not this topology switch
+            if len(switch) != 0:
+                return
+
+            # send cross domain link add
+            self.local_lib.send_cross_domain_link(dst_dpid, dst_port_no, src_dpid, src_port_no)
+
+            # add global port
+            self.global_port.setdefault(dst_dpid, [])
+            self.global_port[dst_dpid].append(dst_port_no)
+
+            return
+        except LLDPPacket.LLDPUnknownFormat:
+            # This handler can receive all the packtes which can be
+            # not-LLDP packet. Ignore it silently
+            pass
+
+        # non-LLDP
+        # local routing
+        dpid = datapath.id
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        src = eth.src
+        dst = eth.dst
+
+        if dst not in self.local_lib.hosts:
+            # can't find host in local topology
+            # ask global and let this msg queued
+            if dst in mDNS or dst in multicast_list:
+                return
+
+            if dst == 'ff:ff:ff:ff:ff:ff':
+                self._flood_packet(msg)
+                return
+            self.route_list.append((dst, msg))
+            self.local_lib.get_route(dst)
+            return
+
+        LOG.debug('Packet in, from %s, to %s', src, dst)
+        # found in local, do local routing
+        # two case:
+        # 1. In same switch
+        # 2. Not in same switch
+        host = self.local_lib.hosts[dst]
+
+        # host[0] -> dpid
+        # host[1] -> port
+        if host[0] == dpid:
+            # same switch
+            out_port = host[1]
+            self._packet_out(msg, out_port)
+
+        else:
+            # not same switch
+            # calculate path
+            self._packet_out_to(msg, host[0], host[1])
+
+    def _get_out_port(self, src, dst):
+        '''
+        get output port from src switch to dst switch
+        return
+        '''
+        links = api.get_all_link(self)
+
+        for link in links:
+
+            if link.src.dpid == src and \
+               link.dst.dpid == dst:
+                return link.src.port_no
+
+    def _flood_packet(self, msg):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        data = None
+
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out_port = ofproto.OFPP_FLOOD
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions,
+                                  data=data)
+        datapath.send_msg(out)
+
+    def _packet_out(self, msg, out_port):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        data = None
+
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions,
+                                  data=data)
+        LOG.debug('packet out to %d:%d', datapath.id, out_port)
+        datapath.send_msg(out)
+
+        # add flow
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dst = eth.dst
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+
+        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+            self._add_flow(datapath, 1, match, actions, msg.buffer_id)
+        else:
+            self._add_flow(datapath, 1, match, actions)
+
+    def _packet_out_to(self, msg, dst_dpid, dst_out_port):
+        dp = msg.datapath
+        src_dpid = dp.id
+
+        # same dp
+        if src_dpid == dst_dpid:
+            self._packet_out(msg, dst_out_port)
+
+        else:
+            g = nx.Graph()
+            links = api.get_all_link(self)
+
+            for link in links:
+                src = link.src.dpid
+                dst = link.dst.dpid
+                g.add_edge(src, dst)
+            src = src_dpid
+            dst = dst_dpid
+            path = None
+
+            if nx.has_path(g, src, dst):
+                path = nx.shortest_path(g, src, dst)
+
+            if path == None:
+                return
+            out_port = self._get_out_port(path[0], path[1])
+            self._packet_out(msg, out_port)
+
+    def _add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(local_lib.EventRouteResult, MAIN_DISPATCHER)
+    def route_result_handler(self, ev):
+        dpid = ev.dpid
+        port = ev.port
+        host = ev.host
+        LOG.info('Receive route result, %d:%d %s', dpid, port, host)
+        remove_list = []
+        if dpid == -1:
+            # global routing failed
+            # do broad cast
+            LOG.debug('Unknown hsot %s', host)
+            for route_req in self.route_list:
+
+                if route_req[0] != host:
+                    continue
+                self._flood_packet(route_req[1])
+                remove_list.append(route_req)
+
+        else:
+            # global routing not failed
+            for route_req in self.route_list:
+
+                if route_req[0] != host:
+                    continue
+                self._packet_out_to(route_req[1], dpid, port)
+                remove_list.append(route_req)
+
+        for remove_item in remove_list:
+
+            try:
+                index = self.route_list.index(remove_item)
+                del self.route_list[index]
+
+            except ValueError:
+                pass
+
+    @set_ev_cls(local_lib.EventAskDpid, MAIN_DISPATCHER)
+    def ask_dpid_handler(self, ev):
+        dpid = ev.dpid
+        switch = api.get_switch(self, dpid)
+
+        if len(switch) != 0:
+            self.local_lib.response_dpid(dpid)
+
+    @set_ev_cls(local_lib.EventAskHost, MAIN_DISPATCHER)
+    def ask_host_handler(self, ev):
+        host_mac = ev.host
+
+        if host_mac in self.local_lib.hosts:
+            self.local_lib.response_host(host_mac)
+
+    @set_ev_cls(local_lib.EventHostDiscovery, MAIN_DISPATCHER)
+    def host_discovery_handler(self, ev):
+        LOG.debug('Discover host %s on %s, port %d', ev.host, ev.dpid, ev.port)
---
+++ flowtime.py
@@ -0,0 +1,154 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import  DEAD_DISPATCHER         ##Ahmed
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+from ryu.lib import hub     ###Ahmed
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.en_clear_flow_entry = False        ####Ahmed
+        self.datapaths = {}                             ####Ahmed
+        self.monitor_thread = hub.spawn(self._monitor)  ##Ahmed
+        
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.logger.debug('register datapath: %016x', datapath.id)
+                self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
+
+        
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                print("dp in handler is ",dp)
+            hub.sleep(10)    
+
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None): #modify flow entry
+        # print ("Adding flow ", match, actions)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        print("en_clear_flow_entry = ",self.en_clear_flow_entry)
+        if buffer_id:
+            if(self.en_clear_flow_entry):
+                mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,idle_timeout=idle_time,#*datapath.id,
+                                    instructions=inst)
+            else:
+                mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            if(self.en_clear_flow_entry):
+                mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, idle_timeout=idle_time,#*datapath.id
+                                    instructions=inst)
+            else:
+                mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match,instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER) #create miss flow entry then send it
+    def _switch_features_handler(self, ev):
+        print ("switch_features_handler is called")
+        datapath = ev.msg.datapath
+        print ("switch_features_handler is called for ",datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        print("actions for called switch miss flow entry is ",actions)                                  
+        self.add_flow(datapath, 0, match, actions)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = format(datapath.id, "d").zfill(16)
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
