3e55afdf0bdd114ff5e4a0083ec56f53caa00c70
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-09-02 07:32:23 +0200

    Delete run_slave.sh

--- MultiControl/ms/run_slave.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- sudo ryu-manager --observe-links --ofp-tcp-listen-port 6633 SlaveApp.py controller.py /home/ubuntu/sdn/sources/flowmanager/flowmanager.py --observe-links --wsapi-port 8081
-
