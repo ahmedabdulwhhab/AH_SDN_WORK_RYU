52df3b38d09aa83ff8777ba6ed8659dd1bcb8e5e
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-09-02 07:32:13 +0200

    Delete run_master.sh

--- MultiControl/ms/run_master.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo ryu-manager --observe-links --ofp-tcp-listen-port 6632 MasterApp.py controller.py /home/ubuntu/sdn/sources/flowmanager/flowmanager.py --observe-links --wsapi-port 8080
