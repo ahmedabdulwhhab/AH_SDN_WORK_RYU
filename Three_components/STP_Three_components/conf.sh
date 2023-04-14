e6e0dee5c05a6536fa8232ecd0283857e93b4649
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2023-04-14 05:59:30 +0200

    Add files via upload

---
+++ Three_components/STP_Three_components/conf.sh
@@ -0,0 +1,20 @@
+
+##################################
+#for router 0000000000000021
+#assign address for each port
+curl -X POST -d '{"address": "192.168.10.1/24"}' http://localhost:8086/router/0000000000000021
+curl -X POST -d '{"address": "192.168.20.1/24"}' http://localhost:8086/router/0000000000000021
+
+#firewall
+curl -X PUT http://127.0.0.1:8086/firewall/module/enable/0000000000000031
+
+curl -X POST -d '{"nw_src": "192.168.10.0/24", "nw_dst": "192.168.20.0/24", "nw_proto": "ICMP"}' http://127.0.0.1:8086/firewall/rules/0000000000000031
+
+curl -X POST -d '{"nw_src": "192.168.20.0/24", "nw_dst": "192.168.10.0/24", "nw_proto": "ICMP"}' http://127.0.0.1:8086/firewall/rules/0000000000000031
+
+#also net 20.x can talk to 20.x
+curl -X POST -d '{"nw_src": "192.168.20.0/24", "nw_dst": "192.168.20.0/24", "nw_proto": "ICMP"}' http://127.0.0.1:8086/firewall/rules/0000000000000031
+
+
+#also net 20.x can talk to 20.x
+#curl -X POST -d '{"nw_src": "192.168.10.0/24", "nw_dst": "192.168.10.0/24", "nw_proto": "ICMP"}' http://127.0.0.1:8086/firewall/rules/0000000000000031
